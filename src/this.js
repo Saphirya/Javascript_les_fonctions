ffaa671a241a2216ef0c7f73007476fdbdf2ed9f
Florian <floriangeorges1992@gmail.com>
2024-11-30 15:23:57 +0100

    add fonctions_flechees.js

--- src/this.js
+++ src/this.js
@@ -1,1 +1,29 @@
 //fonctionnement du keyword this
+import "./test";
+function func() {
+  // console.log(this);
+}
+
+func();
+
+//définir this avec .call() ou .apply() ou .bind()
+
+const b = {
+  name: "Jean",
+  lastName: "Dupont",
+};
+
+function bonjour(lang) {
+  if (lang === "fr") {
+    console.log(`bonjour: ${this.name} ${this.lastName}`);
+  } else {
+    console.log(`Hello: ${this.name + this.lastName}`);
+  }
+}
+bonjour.call(b, "en");
+bonjour.apply(b, ["fr"]);
+
+//bind()
+
+const c = bonjour.bind(b);
+c("en");
